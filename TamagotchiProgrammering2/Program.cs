97614474dfc78130d6cf2bfb854d4a2ac2f47939
Peamseye <aispotato587@gmail.com>
2022-09-15 12:18:47 +0200

    VariablesUndefined

--- TamagotchiProgrammering2/Program.cs
+++ TamagotchiProgrammering2/Program.cs
@@ -1,5 +1,11 @@
-﻿
+﻿using System;
+
 Tamagotchi T1 = new Tamagotchi();
+
+Console.WriteLine("What will your Tamagotchi be called?");
+T1.name = Console.ReadLine();
+
+
 
 T1.PrintStats();
 
--- TamagotchiProgrammering2/Tamagotchi.cs
+++ TamagotchiProgrammering2/Tamagotchi.cs
@@ -2,24 +2,35 @@ using System;
 
 public class Tamagotchi{
 
-private int hunger = 0;
-private int boredom = 0;
-List<string> words = new List<string>();
-private bool isAlive = true;
-private Random generator = new Random();
-public string name = ("");
+private int hunger;
+private int boredom;
+private List<string> words = new List<string>() {"Test"};
+private bool isAlive;
+private Random generator;
+public string name;
+
+public Tamagotchi()
+{
+isAlive = true;
+generator = new Random();
+}
 
 private void ReduceBoredom(){
 boredom--;
 }
 
 public void Feed(){
+Console.WriteLine($"{name}'s hunger decreases.");
 hunger-= 3;
-if (hunger < 0){
-hunger = 0;
+if (hunger < 0){hunger = 0;}
 }
-Console.WriteLine($"You fed {name}.");
-}
+
+public void Hi()
+  {
+    int wordNum = generator.Next(words.Count);
+    ReduceBoredom();
+    Console.WriteLine($"{name} says: {words[wordNum]}");
+  }
 
 public void Tick(){
  hunger += 1;
