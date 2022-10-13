2e926e297c09c7882f6e83bb97ca31c0c35b55dd
Peamseye <aispotato587@gmail.com>
2022-10-13 10:39:33 +0200

    FInallansdlkaw

--- TamagotchiProgrammering2/Tamagotchi.cs
+++ TamagotchiProgrammering2/Tamagotchi.cs
@@ -1,15 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 
 
-public class Tamagotchi{
-
+public class Tamagotchi
+{
 private int hunger;
+
 private int boredom;
+
 private List<string> words = new List<string>() {"Helo"};
+
 private bool isAlive;
+
 private Random generator;
+
 public string name;
+
+
 
 public Tamagotchi()
 {
@@ -17,30 +25,22 @@ generator = new Random();
 isAlive = true;
 }
 
-private void ReduceBoredom(){
-boredom-= 3;
-if (boredom < 0) {boredom = 0;}
-}
-
-public void Feed(){
-Console.WriteLine($"{name}'s hunger decreases.");
-hunger-= 3;
-if (hunger < 0){hunger = 0;}
+public void Feed()
+{
+  Console.WriteLine($"{name}'s hunger decreases.");
+  hunger-= 3;
+  if (hunger < 0)
+  {
+    hunger = 0;
+  }
 }
 
 public void Hi()
   {
     int wordNum = generator.Next(words.Count);
-    Console.WriteLine($"{name} says: {words[wordNum]}");
+    Console.WriteLine($" {name} says: {words[wordNum]}");
     ReduceBoredom();
   }
-
-public void Tick(){
- hunger ++;
- boredom ++; 
-
- if (hunger > 10 || boredom > 10) {isAlive = false;}
-}
 
 public void Teach(string word)
   {
@@ -49,13 +49,33 @@ public void Teach(string word)
     ReduceBoredom();
   }
 
-
-public void PrintStats(){
-    Console.WriteLine($"Name: {name} -|- {hunger} hunger -|- {boredom} boredom -|- Known vocabulary: {words}.");
+public void Tick()
+{
+ hunger ++;
+ boredom ++; 
+ if (hunger > 10 || boredom > 10) 
+ {
+  isAlive = false;
+  }
 }
 
-public bool GetAlive(){
+public void PrintStats()
+{
+    Console.WriteLine($"Name: {name} | {hunger} hunger | {boredom} boredom | Known vocabulary: {words.Count} words.");
+}
+
+public bool GetAlive()
+{
 return isAlive;
 }
 
+public void ReduceBoredom()
+{
+  Console.WriteLine($"{name} is now less bored!");
+boredom-= 3;
+if (boredom < 0) 
+{
+  boredom = 0;
+  }
+ }
 }
