5f42d547cc7d21d72bfc9a86736cda651ebe027a
Peamseye <aispotato587@gmail.com>
2022-10-13 10:16:41 +0200

    Fianll

--- TamagotchiProgrammering2/Tamagotchi.cs
+++ TamagotchiProgrammering2/Tamagotchi.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections.Generic;
 
+
 public class Tamagotchi{
 
 private int hunger;
 private int boredom;
-private List<string> words = new List<string>() {"Hi"};
+private List<string> words = new List<string>() {"Helo"};
 private bool isAlive;
 private Random generator;
 public string name;
